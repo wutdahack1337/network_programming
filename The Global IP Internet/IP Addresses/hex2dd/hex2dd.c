c5329836ba729d6f8adc8f29097691ca9235a1cf
wutdahack1337 <153523415+wutdahack1337@users.noreply.github.com>
2025-02-08 17:08:42 +0700

    Update hex2dd.c

--- The Global IP Internet/IP Addresses/hex2dd/hex2dd.c
+++ The Global IP Internet/IP Addresses/hex2dd/hex2dd.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
-int main(){
+int main(int argc, char* args[]){
+  if (argc != 2){
+    printf("./hex2dd <Network Bytes>\n");
+    return 1;
+  }
 
+  unsigned int n;
+  if (sscanf(args[1], "%x", n) == 0){
+    printf("[!] Something wrong!!!");
+    return 1;
+  } 
+  printf("[!] Got n = 0x%x\n", n);
+
+  char* ipBuffer[105];
+  // chuyen tu byte sang string
+  ///
+  
   return 0;
 }
